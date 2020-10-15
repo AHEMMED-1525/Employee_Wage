d755ca7141705f760e9047630234953920c7a451
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-15 17:01:32 +0530

    Checked Whether the Employee is present or Absent

---
+++ EmpWage.java
@@ -0,0 +1,12 @@
+public class EmpWage{
+	public static void main(String[] args){
+		int is_Present=1;
+		double EmpChck=Math.floor(Math.random()*2)%2;
+		      if(EmpChck==is_Present){
+         System.out.println("Employee is present");
+         }
+      else
+         System.out.println("Employee is Absent");
+
+	}
+}
